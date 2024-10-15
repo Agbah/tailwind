71831ad0d8cab2a2473d3dcb3ed5331dc4f19086
Agbah Bright <agbahb33@gmail.com>
2024-10-15 17:26:16 +0000

    Initial commit for Tailwind project

---
+++ tailwind.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+  content: [
+    'C:/Users/sedem/Desktop/tailwind/src/**/*.{html,js}', // Adjust according to your project structure
+    './index.html',
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
